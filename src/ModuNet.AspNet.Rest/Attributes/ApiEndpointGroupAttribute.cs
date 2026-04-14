6b624eb3ad9b5472f72f2dc5f4d0ddf11561fbc4
Stanislav Ciobanu <50024402+Rengeka@users.noreply.github.com>
2026-04-14 19:32:59 +0300

    Deploy pipeline (#1)
    * Pipeline fix
    
    * Pipeline changed

--- src/ModuNet.AspNet.Rest/Attributes/ApiEndpointGroupAttribute.cs
+++ src/ModuNet.AspNet.Rest/Attributes/ApiEndpointGroupAttribute.cs
@@ -1,6 +1,18 @@
 ﻿namespace ModuNet.AspNet.Rest.Attributes;
 
+/// <summary>
+/// Marks a class as an API endpoint group and defines its base route.
+/// </summary>
+/// <remarks>
+/// Classes with this attribute are automatically discovered
+/// by <see cref="EndpointRouteBuilderExtensions.MapEndpointGroups"/> 
+/// and mapped to the routing system.
+/// Classes marked by this attribute must implement <see cref="IEndpointGroup">
+/// </remarks>
 public class ApiEndpointGroupAttribute(string route) : Attribute
 {
+    /// <summary>
+    /// Gets or sets the base route for the endpoint group.
+    /// </summary>
     public string Route { get; set; } = route;
 }
