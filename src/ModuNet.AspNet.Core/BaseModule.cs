a451ab3553dc63531de23eb0fbac083be25b64d9
Rengeka <stasik2004x@gmail.com>
2026-04-14 19:27:53 +0300

    Pipeline fix

--- src/ModuNet.AspNet.Core/BaseModule.cs
+++ src/ModuNet.AspNet.Core/BaseModule.cs
@@ -3,9 +3,33 @@ using ModuNet.Core.Interfaces;
 
 namespace ModuNet.AspNet.Core;
 
-
+/// <summary>
+/// Base module class that provides common functionality for creating scopes and handling commands and queries.
+/// </summary>
+/// <remarks>
+/// This class creates a service scope and handles the appropriate requests
+/// (<see cref="ICommandHandler{TCommand}"/> and
+/// <see cref="IQueryHandler{TQuery, TResult}"/>).
+/// </remarks>
+/// <param name="moduleScopeFactory"></param>
 public abstract class BaseModule(IModuleScopeFactory moduleScopeFactory) : IModule
 {
+    /// <summary>
+    /// Executes a command asynchronously by resolving the corresponding
+    /// command handler from the dependency injection container.
+    /// </summary>
+    /// <typeparam name="TCommand">
+    /// The type of the command.
+    /// </typeparam>
+    /// <param name="command">
+    /// The command instance to be executed.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token used to cancel the asynchronous operation.
+    /// </param>
+    /// <returns>
+    /// An <see cref="IResponse"/> representing the result of the command execution.
+    /// </returns>
     public async Task<IResponse> ExecuteCommandAsync<TCommand>(TCommand command, CancellationToken cancellationToken)
     {
         var serviceScope = moduleScopeFactory.CreateScope();
@@ -17,6 +41,27 @@ public abstract class BaseModule(IModuleScopeFactory moduleScopeFactory) : IModu
         return response;
     }
 
+    /// <summary>
+    /// Executes a query asynchronously and returns a strongly typed result
+    /// by resolving the corresponding query handler from the
+    /// dependency injection container.
+    /// </summary>
+    /// <typeparam name="TQuery">
+    /// The type of the query.
+    /// </typeparam>
+    /// <typeparam name="TResult">
+    /// The type of the query result.
+    /// </typeparam>
+    /// <param name="request">
+    /// The query instance to be executed.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token used to cancel the asynchronous operation.
+    /// </param>
+    /// <returns>
+    /// An <see cref="IResponse{TResult}"/> representing the result
+    /// of the query execution.
+    /// </returns>
     public async Task<IResponse<TResult>> ExecuteQueryAsync<TQuery, TResult>(TQuery request, CancellationToken cancellationToken)
     {
         var serviceScope = moduleScopeFactory.CreateScope();
--- src/ModuNet.AspNet.Rest/Extentions/EndpointRouteBuilderExtentions.cs
+++ src/ModuNet.AspNet.Rest/Extentions/EndpointRouteBuilderExtentions.cs
@@ -5,8 +5,32 @@ using System.Reflection;
 
 namespace ModuNet.AspNet.Rest.Extentions;
 
+/// <summary>
+/// This class provides extension methods for mapping API endpoint groups
+/// discovered via reflection.
+/// </summary>
+/// <remarks>
+/// This extension scans the specified assembly for concrete types
+/// implementing <see cref="IEndpointGroup"/> and decorated with
+/// <see cref="ApiEndpointGroupAttribute"/>. Each discovered group is
+/// instantiated and mapped to the endpoint routing system.
+/// </remarks>
 public static class EndpointRouteBuilderExtensions
 {
+    /// <summary>
+    /// Discovers and maps all endpoint groups defined in the specified assembly.
+    /// </summary>
+    /// <param name="builder">
+    /// The <see cref="IEndpointRouteBuilder"/> used to configure endpoint routing.
+    /// </param>
+    /// <param name="assembly">
+    /// The assembly to scan for endpoint group implementations.
+    /// </param>
+    /// <remarks>
+    /// Only non-abstract classes implementing <see cref="IEndpointGroup"/> and
+    /// decorated with <see cref="ApiEndpointGroupAttribute"/> are considered.
+    /// Each group is instantiated using its parameterless constructor.
+    /// </remarks>
     public static void MapEndpointGroups(this IEndpointRouteBuilder builder, Assembly assembly)
     {
         var endpointGroupTypes = assembly.GetTypes()
