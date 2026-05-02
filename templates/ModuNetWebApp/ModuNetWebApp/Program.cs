43e412fb159ea268638a9642c9e944cd70cc39a2
Liudmila Ciobanu <stasciobanuwork@gmail.com>
2026-05-02 21:32:21 +0300

    Fixed deployemnt

--- templates/ModuNetWebApp/ModuNetWebApp/Program.cs
+++ templates/ModuNetWebApp/ModuNetWebApp/Program.cs
@@ -14,7 +14,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseWeatherModule();
